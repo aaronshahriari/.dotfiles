df5e0abbb814d32df37c0336ddb6c8ec6451527f
aaronshahriari <aaronshahriari@gmail.com>
2024-08-06 04:26:54 -0400

    update i3 and polybar

--- i3/polybar/launch.sh
+++ i3/polybar/launch.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
-polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
 pkill polybar
-
-# Launch bar1 and bar2
-polybar bar 2>&1 | tee -a /tmp/polybar1.log & disown
-
-echo "Bars launched..."
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload bar &
+  done
+else
+  polybar --reload bar &
+fi
