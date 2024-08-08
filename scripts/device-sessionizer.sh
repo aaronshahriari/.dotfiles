046aee46f20b384d9844945824bba8ab57bfc947
aaronshahriari <aaronshahriari@gmail.com>
2024-08-08 16:31:24 -0400

    update i3lock for x-display

--- scripts/device-sessionizer.sh
+++ scripts/device-sessionizer.sh
@@ -3,7 +3,7 @@
 case "$(printf "Shutdown\nReboot\nLock\nExit i3\n" | dmenu -l 10 -nb '#000000' -sf '#000000' -sb '#67A86C' -nf '#67A86C' -fn 'JetbrainsMono-11' -p "System:")" in
     "Shutdown") sudo poweroff ;;
     "Reboot") sudo reboot ;;
-    "Lock") sudo slock ;;
+    "Lock") i3lock -t -i ~/Downloads/i3lock.png ;;
     "Exit i3") sudo pkill i3 ;;
     *) exit 1 ;;
 esac
