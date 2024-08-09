79c7e5f2d6752e86cc187d0bede508da8b0644cb
aaronshahriari <aaronshahriari@gmail.com>
2024-08-09 13:57:37 -0400

    removed sudo from reboot

--- scripts/device-sessionizer.sh
+++ scripts/device-sessionizer.sh
@@ -2,7 +2,7 @@
 
 case "$(printf "Shutdown\nReboot\nLock\nExit i3\n" | dmenu -l 10 -nb '#000000' -sf '#000000' -sb '#67A86C' -nf '#67A86C' -fn 'CaskaydiaCoveNerdFontMono-10' -p "System:")" in
     "Shutdown") sudo poweroff ;;
-    "Reboot") sudo reboot ;;
+    "Reboot") reboot ;;
     "Lock") ~/.local/bin/scripts/i3lock.sh ;;
     "Exit i3") sudo pkill i3 ;;
     *) exit 1 ;;
