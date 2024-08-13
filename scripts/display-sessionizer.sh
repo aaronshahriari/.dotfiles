4c8f6e323f91de9320b640cbf1630212203154f8
aaronshahriari <aaronshahriari@gmail.com>
2024-08-13 03:52:18 -0400

    updated: added xinput to dmenu setup

--- scripts/display-sessionizer.sh
+++ scripts/display-sessionizer.sh
@@ -3,8 +3,7 @@
 case "$(printf "Home\nLaptop\nSingle-Display\n" | dmenu -l 10 -nb '#000000' -sf '#000000' -sb '#67A86C' -nf '#67A86C' -fn 'CaskaydiaCoveNerdFontMono-10' -p "Display Setup:")" in
     "Home")
         autorandr --load home ;;
-        # testing this in xsession
-        # xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8 ;;
+        xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8 ;;
     # "Laptop")
     #     autorandr --load laptop ;;
     # "Single-Display")
