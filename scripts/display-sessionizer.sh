9e1183b09bb2b6adb403c7bf0e19b5ffee1ae308
aaronshahriari <aaronshahriari@gmail.com>
2024-08-13 03:54:42 -0400

    display sessionizer

--- scripts/display-sessionizer.sh
+++ scripts/display-sessionizer.sh
@@ -2,7 +2,7 @@
 
 case "$(printf "Home\nLaptop\nSingle-Display\n" | dmenu -l 10 -nb '#000000' -sf '#000000' -sb '#67A86C' -nf '#67A86C' -fn 'CaskaydiaCoveNerdFontMono-10' -p "Display Setup:")" in
     "Home")
-        autorandr --load home ;;
+        autorandr --load home
         xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8 ;;
     # "Laptop")
     #     autorandr --load laptop ;;
