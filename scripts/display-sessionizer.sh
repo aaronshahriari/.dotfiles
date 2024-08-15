9ecdd1d5b094bc3dd557a822ee10b000fd4de255
aaronshahriari <aaronshahriari@gmail.com>
2024-08-15 19:12:48 -0400

    autorandr home scripts update

--- scripts/display-sessionizer.sh
+++ scripts/display-sessionizer.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-case "$(printf "Home\nLaptop\nSingle-Display\n" | dmenu -l 10 -nb '#000000' -sf '#000000' -sb '#67A86C' -nf '#67A86C' -fn 'CaskaydiaCoveNerdFontMono-10' -p "Display Setup:")" in
+case "$(printf "Home\n" | dmenu -l 10 -nb '#000000' -sf '#000000' -sb '#67A86C' -nf '#67A86C' -fn 'CaskaydiaCoveNerdFontMono-10' -p "Display Setup:")" in
     "Home")
         case "$(printf "Laptop\nSingle\nDouble\n" | dmenu -l 10 -nb '#000000' -sf '#000000' -sb '#67A86C' -nf '#67A86C' -fn 'CaskaydiaCoveNerdFontMono-10' -p "Display Setup:")" in
             # "Laptop")
             #     autorandr --load laptop
             #     xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8 ;;
             "Single")
+                # set auto setup
                 autorandr --load home-single
+                # set dpi for home setup
+                xrandr --dpi 95
+                # set xinput for home mouse
                 xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8 ;;
             "Double")
                 autorandr --load home-two
