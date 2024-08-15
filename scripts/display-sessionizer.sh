4ccef120385e020c3c956991a327d1a21c9cd1d0
aaronshahriari <aaronshahriari@gmail.com>
2024-08-15 18:44:30 -0400

    autorandr home; update to i3

--- scripts/display-sessionizer.sh
+++ scripts/display-sessionizer.sh
@@ -2,8 +2,16 @@
 
 case "$(printf "Home\nLaptop\nSingle-Display\n" | dmenu -l 10 -nb '#000000' -sf '#000000' -sb '#67A86C' -nf '#67A86C' -fn 'CaskaydiaCoveNerdFontMono-10' -p "Display Setup:")" in
     "Home")
-        autorandr --load home
-        xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8 ;;
+        case "$(printf "Laptop\nSingle\nDouble\n" | dmenu -l 10 -nb '#000000' -sf '#000000' -sb '#67A86C' -nf '#67A86C' -fn 'CaskaydiaCoveNerdFontMono-10' -p "Display Setup:")" in
+            # "Laptop")
+            #     autorandr --load laptop
+            #     xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8 ;;
+            "Single")
+                autorandr --load home-single
+                xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8 ;;
+            "Double")
+                autorandr --load home-two
+                xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8 ;;
     # "Laptop")
     #     autorandr --load laptop ;;
     # "Single-Display")
