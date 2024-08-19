445c397f0a24cbda8547de18ac8f9469ada7deee
aaronshahriari <aaronshahriari@gmail.com>
2024-08-19 16:55:30 -0400

    readme update

--- scripts/device-sessionizer.sh
+++ scripts/device-sessionizer.sh
@@ -2,10 +2,9 @@
 
 source ~/.local/bin/scripts/themes.sh
 
-case "$(printf "Shutdown\nReboot\nLock\nExit i3\n" | dmenu -l 10 -nb $BLACK -sf $BLACK -sb $MAIN -nf $MAIN -fn $FONT -p "System:")" in
+case "$(printf "Shutdown\nReboot\nLock\n" | dmenu -l 10 -nb $BLACK -sf $BLACK -sb $MAIN -nf $MAIN -fn $FONT -p "System:")" in
     "Shutdown") sudo poweroff ;;
     "Reboot") reboot ;;
     "Lock") ~/.local/bin/scripts/i3lock.sh ;;
-    # "Exit i3") sudo pkill i3 ;;
     *) exit 1 ;;
 esac
