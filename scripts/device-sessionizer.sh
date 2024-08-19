b014e108aa61f601672d8c58c2c8c766ba711760
aaronshahriari <aaronshahriari@gmail.com>
2024-08-19 15:14:16 -0400

    removed i3 kill

--- scripts/device-sessionizer.sh
+++ scripts/device-sessionizer.sh
@@ -6,6 +6,6 @@ case "$(printf "Shutdown\nReboot\nLock\nExit i3\n" | dmenu -l 10 -nb $BLACK -sf 
     "Shutdown") sudo poweroff ;;
     "Reboot") reboot ;;
     "Lock") ~/.local/bin/scripts/i3lock.sh ;;
-    "Exit i3") sudo pkill i3 ;;
+    # "Exit i3") sudo pkill i3 ;;
     *) exit 1 ;;
 esac
