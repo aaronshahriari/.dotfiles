a90f1e8bc529709105d74b8aeceb173f63d49f76
aaronshahriari <aaronshahriari@gmail.com>
2024-08-25 11:54:39 -0400

    updated device-sessionizer with reorder and sleep & lock functionality

--- scripts/device-sessionizer.sh
+++ scripts/device-sessionizer.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
-case "$(printf "Shutdown\nReboot\nLock\n" | dmenu -i -l 10 -p "System:")" in
-    "Shutdown")
-        case "$(printf "Yes\nNo" | dmenu -i -l 10 -p "Are You Sure:")" in
-            "Yes") sudo shutdown -h now ;;
-            *) exit 1 ;;
-        esac ;;
+case "$(printf "Lock\nSleep\nReboot\nShutdown\n" | dmenu -i -l 10 -p "System:")" in
+    "Lock") slock ;;
+    "Sleep")
+        systemctl suspend
+        slock
+        ;;
     "Reboot")
         case "$(printf "Yes\nNo" | dmenu -i -l 10 -p "Are You Sure:")" in
             "Yes") reboot ;;
             *) exit 1 ;;
         esac ;;
-    "Lock")
-        systemctl suspend
-        slock
-        ;;
+    "Shutdown")
+        case "$(printf "Yes\nNo" | dmenu -i -l 10 -p "Are You Sure:")" in
+            "Yes") sudo shutdown -h now ;;
+            *) exit 1 ;;
+        esac ;;
     *) exit 1 ;;
 esac
