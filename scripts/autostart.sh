2244e629b9cc6bfcb8d7b49a8c097109cda32aa9
aaronshahriari <aaronshahriari@gmail.com>
2024-08-27 06:19:36 -0400

    update

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-# feh --bg-fill ~/.config/wallpapers/desktop_wall.png --bg-fill ~/.config/wallpapers/desktop_wall.png
+feh --bg-fill ~/.config/wallpapers/desktop_wall.png --bg-fill ~/.config/wallpapers/desktop_wall.png
 xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8
