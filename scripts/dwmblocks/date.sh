02430748c5ab35f2841916562eb1aca222e522cc
aaronshahriari <aaronshahriari@gmail.com>
2024-08-27 04:17:35 -0400

    updatedate blocks

--- scripts/dwmblocks/date.sh
+++ scripts/dwmblocks/date.sh
@@ -4,11 +4,5 @@ case $BLOCK_BUTTON in
 	6) setsid -f "alacritty" -e "nvim" "$0" ;;
 esac
 
-# wait until exactly a minute
-while [ $(date +%S) -ne 00 ]; do
-    sleep 0.1
-done
-
 date_str=$(date "+%D %I:%M%p")
-
 printf "^c#ffffff^%s ^d^" "$date_str"
