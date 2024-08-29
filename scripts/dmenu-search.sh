de962001f71e72315f0b5348203a0a3d338f19fa
aaronshahriari <aaronshahriari@gmail.com>
2024-08-29 04:52:25 -0400

    update dmenu search

--- scripts/dmenu-search.sh
+++ scripts/dmenu-search.sh
@@ -3,7 +3,7 @@
 #!/bin/bash
 
 # Prompt the user with dmenu to enter a search query
-query=$(echo "" | dmenu  -l 0 -g 1 -p "Search:")
+query=$(echo "" | dmenu  -l 0 -g 0 -p "Search:")
 
 # If a query was entered, open Firefox with the search query
 if [ -n "$query" ]; then
