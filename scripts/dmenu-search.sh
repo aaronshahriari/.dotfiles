b2deb3c51ff87a4d1cd7015ddb395a4c87a003af
aaronshahriari <aaronshahriari@gmail.com>
2024-08-30 06:14:25 -0400

    updated with just --search

--- scripts/dmenu-search.sh
+++ scripts/dmenu-search.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-#!/bin/bash
-
-# Prompt the user with dmenu to enter a search query
+# prompt the user with dmenu to enter a search query
 query=$(echo "" | dmenu  -l 0 -g 0 -p "Search:")
 
-# If a query was entered, open Firefox with the search query
+# if a query was entered, open firefox with the search query
 if [ -n "$query" ]; then
-    firefox "https://duckduckgo.com/?q=$query"
+    # firefox "https://duckduckgo.com/?q=$query"
+    firefox --search "$query"
 fi
