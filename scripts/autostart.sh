283cba7e827ef105b4ac9bfdcc0cb2badcbc3019
aaronshahriari <aaronshahriari@gmail.com>
2024-08-30 06:52:09 -0400

    updated wallpaper test

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-feh --bg-fill /usr/share/pixmaps/desktop_wall.png --bg-fill /usr/share/pixmaps/desktop_wall.png &
+feh --bg-fill /usr/share/pixmaps/test_home_wall.jpg --bg-fill /usr/share/pixmaps/test_home_wall.jpg &
 xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8 &
