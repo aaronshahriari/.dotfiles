6646f8a7692e25e91066fc4ded0e5c5e536cd0e5
aaronshahriari <aaronshahriari@gmail.com>
2024-09-07 05:24:17 -0400

    updated autostart with xsetroot cursor

---
+++ scripts/autostart.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+autorandr --load home-two
+feh --bg-fill /usr/share/pixmaps/desktop_wall.jpg --bg-fill /usr/share/pixmaps/desktop_wall.jpg
+xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8
+xsetroot -cursor_name left_ptr
