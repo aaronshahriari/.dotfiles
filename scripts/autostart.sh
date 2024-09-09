8d7aab43967a63ea3be48327e1a02355d60d8421
aaronshahriari <aaronshahriari@gmail.com>
2024-09-09 04:02:13 -0400

    updated autostart

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-# autorandr --load home-two
-# feh --bg-fill /usr/share/pixmaps/desktop_wall.jpg --bg-fill /usr/share/pixmaps/desktop_wall.jpg
+feh --bg-fill /usr/share/backgrounds/desktop_wall.jpg --bg-fill /usr/share/backgrounds/desktop_wall.jpg
 xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8
-xsetroot -cursor_name left_ptr
