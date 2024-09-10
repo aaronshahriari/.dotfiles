7f09c1cdb54cd77ec2c520a96fac2bd3879d0e70
aaronshahriari <aaronshahriari@gmail.com>
2024-09-10 05:07:26 -0400

    flameshot screenshot script

---
+++ scripts/flameshot-screenshot.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# set the directory to save the screenshot
+SCREENSHOT_DIR="/home/aaronshahriari/Pictures/Screenshots/"
+
+# take a screenshot without notifications and save it in the directory
+flameshot gui -c -p $SCREENSHOT_DIR
+
+# find the most recently created file in the screenshot directory that was modified within the last 5 seconds
+LATEST_SCREENSHOT=$(find $SCREENSHOT_DIR -type f -mmin -0.0333 -print | sort -r | head -n 1)
+
+# check if a screenshot file was found and its timestamp
+if [[ -n $LATEST_SCREENSHOT ]]; then
+    # Check if the file exists and is not empty
+    if [[ -f $LATEST_SCREENSHOT && -s $LATEST_SCREENSHOT ]]; then
+        notify-send "Screenshot Taken" -i "$LATEST_SCREENSHOT"
+    else
+        notify-send "Screenshot Error" "The screenshot file is empty or couldn't be saved."
+    fi
+else
+    notify-send "Screenshot Cancelled"
+fi
