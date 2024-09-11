047af28903502385afedba027ed63217aa6fab5b
aaronshahriari <aaronshahriari@gmail.com>
2024-09-11 04:58:15 -0400

    wording

--- scripts/dmenu-search.sh
+++ scripts/dmenu-search.sh
@@ -2,13 +2,13 @@
 
 # prompt the user with dmenu to enter a search query
 case "$(printf "win\ntab\n" | dmenu -m 0 -i -l 10 -p "Window Type:")" in
-    "win")
+    "Window")
         query=$(echo "" | dmenu -m 0 -l 0 -g 0 -p "Search:")
         if [ -n "$query" ]; then
             firefox --search "$query"
         fi
         ;;
-    "tab")
+    "Tab")
         query=$(echo "" | dmenu -m 0 -l 0 -g 0 -p "Search:")
         if [ -n "$query" ]; then
             firefox --new-tab "https://duckduckgo.com/?q=$query"
