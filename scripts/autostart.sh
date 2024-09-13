b16c4a5a12575aa9acdd7c4f215165d1497c36c8
aaronshahriari <aaronshahriari@gmail.com>
2024-09-13 04:11:25 -0400

    updating xinput

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -3,4 +3,5 @@
 autorandr --load home-two
 feh --bg-fill /usr/share/backgrounds/desktop_wall.jpg --bg-fill /usr/share/backgrounds/desktop_wall.jpg
 xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8
+xinput set-prop "pointer:PIXA3854:00 093A:0274 Touchpad" "libinput Tapping Enabled" 0
 xsetroot -cursor_name left_ptr
--- scripts/display-sessionizer.sh
+++ scripts/display-sessionizer.sh
@@ -3,25 +3,21 @@
 case "$(printf "Home\n" | dmenu -m 0 -i -l 10 -p "Display Setup:")" in
     "Home")
         case "$(printf "Laptop\nSingle\nDouble\n" | dmenu -m 0 -i -l 10 -p "Display Setup:")" in
-            # "Laptop")
-            #     autorandr --load laptop
-            #     xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8 ;;
+            "Laptop")
+                autorandr --load laptop
+                xinput set-prop "pointer:PIXA3854:00 093A:0274 Touchpad" "libinput Tapping Enabled" 0
+                xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8 ;;
             "Single")
                 # set auto setup
                 autorandr --load home-single
                 # set dpi for home setup
                 xrandr --dpi 95
                 # set xinput for home mouse
-                xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8
-                # restart i3 to setup wallpaper/polybar/workspaces
-                i3-msg restart ;;
+                xinput set-prop "pointer:PIXA3854:00 093A:0274 Touchpad" "libinput Tapping Enabled" 0
+                xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8 ;;
             "Double")
                 autorandr --load home-two
-                xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8
-                i3-msg restart ;;
+                xinput set-prop "pointer:PIXA3854:00 093A:0274 Touchpad" "libinput Tapping Enabled" 0
+                xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8 ;;
         esac
-    # "Laptop")
-    #     autorandr --load laptop ;;
-    # "Single-Display")
-    #     autorandr --load single ;;
 esac
