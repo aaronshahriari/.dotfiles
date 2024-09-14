8ba01817d6671ced091cec04be6725b22f1e4d7d
aaronshahriari <aaronshahriari@gmail.com>
2024-09-14 10:31:14 -0400

    adding scroll speed for trackpad to be slower

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -4,4 +4,5 @@ autorandr --load home-two
 feh --bg-fill /usr/share/backgrounds/desktop_wall.jpg --bg-fill /usr/share/backgrounds/desktop_wall.jpg
 xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8
 xinput set-prop "pointer:PIXA3854:00 093A:0274 Touchpad" "libinput Tapping Enabled" 0
+xinput set-prop "pointer:PIXA3854:00 093A:0274 Touchpad" "libinput Scrolling Pixel Distance" 50
 xsetroot -cursor_name left_ptr
--- scripts/display-sessionizer.sh
+++ scripts/display-sessionizer.sh
@@ -6,6 +6,7 @@ case "$(printf "Home\n" | dmenu -m 0 -i -l 10 -p "Display:")" in
             "Laptop")
                 autorandr --load laptop
                 feh --bg-fill /usr/share/backgrounds/desktop_wall.jpg --bg-fill /usr/share/backgrounds/desktop_wall.jpg
+                xinput set-prop "pointer:PIXA3854:00 093A:0274 Touchpad" "libinput Scrolling Pixel Distance" 50
                 xinput set-prop "pointer:PIXA3854:00 093A:0274 Touchpad" "libinput Tapping Enabled" 0
                 xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8 ;;
             # "Single")
@@ -13,11 +14,13 @@ case "$(printf "Home\n" | dmenu -m 0 -i -l 10 -p "Display:")" in
             #     autorandr --load home-single
             #     feh --bg-fill /usr/share/backgrounds/desktop_wall.jpg --bg-fill /usr/share/backgrounds/desktop_wall.jpg
             #     # set dpi for home setup
+            #     xinput set-prop "pointer:PIXA3854:00 093A:0274 Touchpad" "libinput Scrolling Pixel Distance" 50
             #     xinput set-prop "pointer:PIXA3854:00 093A:0274 Touchpad" "libinput Tapping Enabled" 0
             #     xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8 ;;
             "Double")
                 autorandr --load home-two
                 feh --bg-fill /usr/share/backgrounds/desktop_wall.jpg --bg-fill /usr/share/backgrounds/desktop_wall.jpg
+                xinput set-prop "pointer:PIXA3854:00 093A:0274 Touchpad" "libinput Scrolling Pixel Distance" 50
                 xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8 ;;
         esac
 esac
