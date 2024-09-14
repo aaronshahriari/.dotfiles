07869cd71d31b688cc8e0b76ace842d631e7206c
aaronshahriari <aaronshahriari@gmail.com>
2024-09-14 17:47:12 -0400

    updated alacritty with new configs with larger text size; also created script to run the correct alacritty with config

---
+++ scripts/alacritty.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+curr_setup="$(autorandr --current)"
+
+if [ "$curr_setup" = "home-two" ]; then
+    alacritty --config-file $HOME/.config/alacritty/home-two.toml
+elif [ "$curr_setup" = "laptop" ]; then
+    alacritty --config-file $HOME/.config/alacritty/laptop.toml
+fi
