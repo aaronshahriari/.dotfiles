8dff44fe587c921b37f1de65a626f229ff144c06
aaronshahriari <aaronshahriari@gmail.com>
2024-09-17 19:59:05 -0400

    updated search to always be window

---
+++ scripts/deprecated/dmenu-search.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# prompt the user with dmenu to enter a search query
+case "$(printf "Window\nTab\n" | dmenu -m 0 -i -l 10 -p "Window Type:")" in
+    "Window")
+        query=$(echo "" | dmenu -m 0 -l 0 -g 0 -p "Search:")
+        if [ -n "$query" ]; then
+            firefox --search "$query"
+        fi
+        ;;
+    "Tab")
+        query=$(echo "" | dmenu -m 0 -l 0 -g 0 -p "Search:")
+        if [ -n "$query" ]; then
+            firefox --new-tab "https://duckduckgo.com/?q=$query"
+        fi
+        ;;
+esac
--- scripts/dmenu-search.sh
+++ scripts/dmenu-search.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 
+#!/usr/bin/env bash
+
 # prompt the user with dmenu to enter a search query
-case "$(printf "Window\nTab\n" | dmenu -m 0 -i -l 10 -p "Window Type:")" in
-    "Window")
-        query=$(echo "" | dmenu -m 0 -l 0 -g 0 -p "Search:")
-        if [ -n "$query" ]; then
-            firefox --search "$query"
-        fi
-        ;;
-    "Tab")
-        query=$(echo "" | dmenu -m 0 -l 0 -g 0 -p "Search:")
-        if [ -n "$query" ]; then
-            firefox --new-tab "https://duckduckgo.com/?q=$query"
-        fi
-        ;;
-esac
+query=$(echo "" | dmenu -m 0 -l 0 -g 0 -p "Search:")
+
+# if a query was entered, open firefox with the search query
+if [ -n "$query" ]; then
+    # firefox "https://duckduckgo.com/?q=$query"
+    firefox --search "$query"
+fi
