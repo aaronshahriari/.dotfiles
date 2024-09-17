85c421be69a66c3a9c366a6380a0d144387f2435
aaronshahriari <aaronshahriari@gmail.com>
2024-09-16 20:10:56 -0400

    updated alacritty config else statement to use default alacritty

--- scripts/alacritty.sh
+++ scripts/alacritty.sh
@@ -6,4 +6,6 @@ if [ "$curr_setup" = "home-two" ]; then
     alacritty --config-file $HOME/.config/alacritty/home-two.toml
 elif [ "$curr_setup" = "laptop" ]; then
     alacritty --config-file $HOME/.config/alacritty/laptop.toml
+else
+    alacritty --config-file $HOME/.config/alacritty/alacritty.toml
 fi
