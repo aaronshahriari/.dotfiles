349cec980f31c5535041b7d788f4d15ccd8a5f42
aaronshahriari <aaronshahriari@gmail.com>
2024-09-18 04:22:30 -0400

    added trouble and updated keymap to use <C-x> to toggle

---
+++ nvim/after/plugin/trouble.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n", "<C-x>", "<cmd>TroubleToggle quickfix<cr>",
+  {silent = true, noremap = true}
+)
--- nvim/lua/aaronshahriari/lazy.lua
+++ nvim/lua/aaronshahriari/lazy.lua
@@ -19,6 +19,7 @@ require('lazy').setup({
     --         "ibhagwan/fzf-lua",
     --     }
     -- },
+    { "folke/trouble.nvim" },
     {
         "hrsh7th/nvim-cmp",
         lazy = false,
