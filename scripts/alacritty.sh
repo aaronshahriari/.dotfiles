0fa72786db94dad8055826838e23e35fc064ce5a
aaronshahriari <aaronshahriari@gmail.com>
2024-09-23 05:27:39 -0400

    updated alacritty with new logic

--- scripts/alacritty.sh
+++ scripts/alacritty.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-curr_setup="$(autorandr --current)"
-detect_setup="$(autorandr --detected)"
+alacritty_status=/tmp/alacritty
+curr_setup="$(cat "$alacritty_status")"
 
-if [ "$curr_setup" = "home-two" ] || [ "$detect_setup" = "home-two" ]; then
+if [ "$curr_setup" = "home-two" ]; then
     alacritty --config-file $HOME/.config/alacritty/home-two.toml
-elif [ "$curr_setup" = "laptop" ] || [ "$detect_setup" = "laptop" ]; then
+elif [ "$curr_setup" = "laptop" ]; then
     alacritty --config-file $HOME/.config/alacritty/laptop.toml
 else
     alacritty --config-file $HOME/.config/alacritty/alacritty.toml
