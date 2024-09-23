1257a94702ef4f964ae18ee5139e179d566c6b9b
aaronshahriari <aaronshahriari@gmail.com>
2024-09-23 04:16:22 -0400

    updated detected or current display setup then use alacritty

--- scripts/alacritty.sh
+++ scripts/alacritty.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 curr_setup="$(autorandr --current)"
+detect_setup="$(autorandr --detected)"
 
-if [ "$curr_setup" = "home-two" ]; then
+if [ "$curr_setup" = "home-two" ] || [ "$detect_setup" = "home-two" ]; then
     alacritty --config-file $HOME/.config/alacritty/home-two.toml
-elif [ "$curr_setup" = "laptop" ]; then
+if [ "$curr_setup" = "laptop" ] || [ "$detect_setup" = "laptop" ]; then
     alacritty --config-file $HOME/.config/alacritty/laptop.toml
 else
     alacritty --config-file $HOME/.config/alacritty/alacritty.toml
