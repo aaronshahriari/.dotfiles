7b3b93db1ba96b59621d7ec1fb1ef2c8552559b4
aaronshahriari <aaronshahriari@gmail.com>
2024-10-04 06:13:24 -0400

    updated with restart dwmblocks if restart something and need to restart

--- scripts/fix-devices.sh
+++ scripts/fix-devices.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-case "$(printf "Wallpaper\nLogitech Mouse\nTouchpad\n" | dmenu -m 0 -i -l 10 -p "Display:")" in
+case "$(printf "Wallpaper\nLogitech Mouse\nTouchpad\nRestart DWMBlocks\n" | dmenu -m 0 -i -l 10 -p "Display:")" in
     "Wallpaper")
         feh --bg-fill /usr/share/backgrounds/desktop_wall.jpg --bg-fill /usr/share/backgrounds/desktop_wall.jpg
         ;;
@@ -10,5 +10,9 @@ case "$(printf "Wallpaper\nLogitech Mouse\nTouchpad\n" | dmenu -m 0 -i -l 10 -p 
     "Touchpad")
         xinput set-prop "pointer:PIXA3854:00 093A:0274 Touchpad" "libinput Scrolling Pixel Distance" 50
         xinput set-prop "pointer:PIXA3854:00 093A:0274 Touchpad" "libinput Tapping Enabled" 0
+        ;;
+    "Restart DWMBlocks")
+        pkill dwmblocks
+        dwmblocks &
         ;;
 esac
