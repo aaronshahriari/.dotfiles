d1bc0b841e27d99cd12a5db7d901978326441f22
aaronshahriari <aaronshahriari@gmail.com>
2024-10-04 04:14:08 -0400

    added new module for device fixing

---
+++ scripts/fix-devices.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+case "$(printf "Wallpaper\nLogitech Mouse\nTouchpad\n" | dmenu -m 0 -i -l 10 -p "Display:")" in
+    "Wallpaper")
+        feh --bg-fill /usr/share/backgrounds/desktop_wall.jpg --bg-fill /usr/share/backgrounds/desktop_wall.jpg
+        ;;
+    "Logitech Mouse")
+        xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8
+        ;;
+    "Touchpad")
+        xinput set-prop "pointer:PIXA3854:00 093A:0274 Touchpad" "libinput Scrolling Pixel Distance" 50
+        xinput set-prop "pointer:PIXA3854:00 093A:0274 Touchpad" "libinput Tapping Enabled" 0
+        ;;
+esac
