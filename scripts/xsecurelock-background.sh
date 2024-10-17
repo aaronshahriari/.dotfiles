83f5d3272ac460509a99470cb676c294bcd4ed28
aaronshahriari <aaronshahriari@gmail.com>
2024-10-17 05:26:40 -0400

    added new module that i need to import for xsecurelock that prints wallpaper to two screens

---
+++ scripts/xsecurelock-background.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+window_id=$XSCREENSAVER_WINDOW
+
+if [ "$XSCREENSAVER_SAVER_INDEX" = "1" ]; then
+  IMAGE_PATH="/usr/share/backgrounds/lockscreen.jpg"
+else
+  IMAGE_PATH="/usr/share/backgrounds/lockscreen.jpg"
+fi
+
+feh \
+  --window-id="$window_id" \
+  --xinerama-index="$XSCREENSAVER_SAVER_INDEX" \
+  --zoom=fill \
+  -F "$IMAGE_PATH" 
