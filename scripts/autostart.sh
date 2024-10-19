0974de529221e8a6935a0f4b33ca47cafe087a69
aaronshahriari <aaronshahriari@gmail.com>
2024-10-19 05:30:31 -0400

    updated with xset and xsecurelock for timeout suspend

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -5,3 +5,5 @@ xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8
 xinput set-prop "pointer:PIXA3854:00 093A:0274 Touchpad" "libinput Tapping Enabled" 0
 xinput set-prop "pointer:PIXA3854:00 093A:0274 Touchpad" "libinput Scrolling Pixel Distance" 50
 xsetroot -cursor_name left_ptr
+xset s 300 5
+xss-lock -l -- xsecurelock
