39410e932bd5941524dee7d82887bd3b678ba2a8
aaronshahriari <aaronshahriari@gmail.com>
2024-10-19 06:08:34 -0400

    updated wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,16 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
+-- create datetime bottom right
+wezterm.on('update-right-status', function(window)
+    local date = wezterm.strftime '%m-%d-%Y %H:%M'
+
+    -- Make it italic and underlined
+    window:set_right_status(wezterm.format {
+        { Text = date },
+    })
+end)
+
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
@@ -8,14 +18,48 @@ local config = wezterm.config_builder()
 -- config.color_scheme = 'Pastel White (terminal.sexy)'
 config.color_scheme = '3024 (base16)'
 
--- tabbar
+config.window_close_confirmation = 'NeverPrompt'
+
+-- TABBAR
 -- config.enable_tab_bar = false
 -- config.hide_tab_bar_if_only_one_tab = true
-config.window_close_confirmation = 'NeverPrompt'
 config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.window_frame = {
+    font = wezterm.font 'CaskaydiaCove Nerd Font Mono',
+    font_size = 12.0,
+    active_titlebar_bg = '#333333',
+    inactive_titlebar_bg = '#333333',
+}
 
 -- keybindings
+
+config.leader = { key = "Space", mods = "CTRL" }
+
 config.keys = {
+    -- tmux like commands to use wezterm
+    { key = "c", mods = "LEADER",       action = wezterm.action { SpawnTab = "CurrentPaneDomain" } },
+    { key = "h", mods = "LEADER",       action = wezterm.action { ActivatePaneDirection = "Left" } },
+    { key = "j", mods = "LEADER",       action = wezterm.action { ActivatePaneDirection = "Down" } },
+    { key = "k", mods = "LEADER",       action = wezterm.action { ActivatePaneDirection = "Up" } },
+    { key = "l", mods = "LEADER",       action = wezterm.action { ActivatePaneDirection = "Right" } },
+    { key = "H", mods = "LEADER|SHIFT", action = wezterm.action { AdjustPaneSize = { "Left", 5 } } },
+    { key = "J", mods = "LEADER|SHIFT", action = wezterm.action { AdjustPaneSize = { "Down", 5 } } },
+    { key = "K", mods = "LEADER|SHIFT", action = wezterm.action { AdjustPaneSize = { "Up", 5 } } },
+    { key = "L", mods = "LEADER|SHIFT", action = wezterm.action { AdjustPaneSize = { "Right", 5 } } },
+    { key = "s", mods = "LEADER",       action = wezterm.action { SplitVertical = { domain = "CurrentPaneDomain" } } },
+    { key = "v", mods = "LEADER",       action = wezterm.action { SplitHorizontal = { domain = "CurrentPaneDomain" } } },
+    { key = "1", mods = "LEADER",       action = wezterm.action { ActivateTab = 0 } },
+    { key = "2", mods = "LEADER",       action = wezterm.action { ActivateTab = 1 } },
+    { key = "3", mods = "LEADER",       action = wezterm.action { ActivateTab = 2 } },
+    { key = "4", mods = "LEADER",       action = wezterm.action { ActivateTab = 3 } },
+    { key = "5", mods = "LEADER",       action = wezterm.action { ActivateTab = 4 } },
+    { key = "6", mods = "LEADER",       action = wezterm.action { ActivateTab = 5 } },
+    { key = "7", mods = "LEADER",       action = wezterm.action { ActivateTab = 6 } },
+    { key = "8", mods = "LEADER",       action = wezterm.action { ActivateTab = 7 } },
+    { key = "9", mods = "LEADER",       action = wezterm.action { ActivateTab = 8 } },
+    { key = "d", mods = "LEADER",       action = wezterm.action { CloseCurrentPane = { confirm = true } } },
+    { key = "x", mods = "LEADER",       action = wezterm.action { CloseCurrentPane = { confirm = true } } },
     {
         key = 'w',
         mods = 'CTRL|SHIFT',
@@ -66,13 +110,5 @@ config.font = wezterm.font 'CaskaydiaCove Nerd Font Mono'
 -- cursor
 config.cursor_blink_rate = 0
 config.hide_mouse_cursor_when_typing = true
-
--- tabs settings
-config.window_frame = {
-    font = wezterm.font 'CaskaydiaCove Nerd Font Mono',
-    font_size = 11.0,
-    active_titlebar_bg = '#333333',
-    inactive_titlebar_bg = '#333333',
-}
 
 return config
