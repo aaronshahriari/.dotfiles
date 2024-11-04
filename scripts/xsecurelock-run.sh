bdc4341a02ccda7577f53e4184e7b545923d9a01
aaronshahriari <aaronshahriari@gmail.com>
2024-11-04 05:46:51 -0500

    added coloring and working version of xss-lock with xsecurelock to run when timer hits

--- scripts/xsecurelock-run.sh
+++ scripts/xsecurelock-run.sh
@@ -1,13 +1,17 @@
-#!/usr/bin/env bash
+# check if an argument is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <arg>"
+    exit 1
+fi
 
-# Source any environment settings, like your zsh profile
-source ~/.config/zsh/.zsh_profile
+# access the argument
+ARG="$1"
 
 # Set all xsecurelock environment variables
 export XSECURELOCK_PASSWORD_PROMPT="asterisks"
 export XSECURELOCK_AUTH_CURSOR_BLINK=0
-export XSECURELOCK_SAVER=~/.local/bin/scripts/xsecurelock-background.sh
 export XSECURELOCK_AUTH_BACKGROUND_COLOR="#337690"
+export XSECURELOCK_BACKGROUND_COLOR="#101833"
 export XSECURELOCK_SINGLE_AUTH_WINDOW=0
 export XSECURELOCK_AUTH_FOREGROUND_COLOR="#000000"
 export XSECURELOCK_SHOW_DATETIME=1
@@ -17,4 +21,5 @@ export XSECURELOCK_SHOW_KEYBOARD_LAYOUT=0
 export XSECURELOCK_DEBUG_WINDOW_INFO=1
 
 # Start xsecurelock with the configured environment
-exec xsecurelock
+# exec xsecurelock
+exec $ARG
