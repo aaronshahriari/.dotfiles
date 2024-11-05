c200431388a1d66d9190d5c3e7052fb846f0f4da
aaronshahriari <aaronshahriari@gmail.com>
2024-11-05 04:29:14 -0500

    updated dmenu web search

--- scripts/dmenu-search.sh
+++ scripts/dmenu-search.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
-#!/usr/bin/env bash
+choice=$(echo -e "New Window\nNew Tab" | dmenu -m 0 -l 2 -p "Open in:")
 
-# prompt the user with dmenu to enter a search query
-query=$(echo "" | dmenu -m 0 -l 0 -g 0 -p "Search:")
-
-# if a query was entered, open firefox with the search query
-if [ -n "$query" ]; then
-    google-chrome-stable "https://duckduckgo.com/?q=$query"
-    # firefox --search "$query"
+if [ -n "$choice" ]; then
+    query=$(echo "" | dmenu -m 0 -l 0 -g 0 -p "Search:")
+    if [ -n "$query" ]; then
+        case "$choice" in
+            "New Window")
+                google-chrome-stable --new-window "https://duckduckgo.com/?q=$query"
+                ;;
+            "New Tab")
+                google-chrome-stable --new-tab "https://duckduckgo.com/?q=$query"
+                ;;
+            *)
+                echo "Invalid choice, exiting." >&2
+                exit 1
+                ;;
+        esac
+    fi
+else
+    echo "No choice made, exiting." >&2
+    exit 1
 fi
