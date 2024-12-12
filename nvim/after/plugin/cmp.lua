16db3933875199a92730a1cb678afaf2272d8b49
aaronshahriari <aaronshahriari@gmail.com>
2024-12-12 04:34:10 -0500

    lua snip added to cmp

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -1,9 +1,14 @@
 local cmp = require "cmp"
 
 cmp.setup {
+    snippet = {
+        expand = function(args)
+            require 'luasnip'.lsp_expand(args.body)
+        end
+    },
     mapping = {
-        ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), {'i','c'}),
-        ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), {'i','c'}),
+        ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
+        ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
     },
     sources = {
