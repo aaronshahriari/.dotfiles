43801cae5b2e76a6c0688c0acb367c0fda59f393
aaronshahriari <aaronshahriari@gmail.com>
2024-12-19 08:34:44 -0500

    updated blink.cmp to remove part of the {}

--- nvim/lua/custom/plugins/blink.lua
+++ nvim/lua/custom/plugins/blink.lua
@@ -1,16 +1,18 @@
 return {
-  "saghen/blink.cmp",
-  dependencies = "rafamadriz/friendly-snippets",
-  version = "v0.*",
-  build = 'nix run .#build-plugin',
-  opts = {
-    keymap = { preset = "default" },
+  {
+    "saghen/blink.cmp",
+    dependencies = "rafamadriz/friendly-snippets",
+    version = "v0.*",
+    -- build = 'nix run .#build-plugin',
+    opts = {
+      keymap = { preset = "default" },
 
-    appearance = {
-      use_nvim_cmp_as_default = true,
-      nerd_font_variant = "mono"
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = "mono"
+      },
+      signature = { enabled = true }
     },
-    signature = { enabled = true }
+    opts_extend = { "sources.default" }
   },
-  opts_extend = { "sources.default" }
 }
