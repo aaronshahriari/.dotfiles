2affdb529ca8fdbd5e7376b3f9b39403382e84cc
aaronshahriari <aaronshahriari@gmail.com>
2024-12-19 07:15:14 -0500

    fully reformatted; issue with blink.cmp though

--- nvim/after/plugin/colorizer.lua
+++
@@ -1,6 +0,0 @@
--- Attaches to every FileType mode
-require 'colorizer'.setup({
-    'lua',
-    css = { rgb_fn = true, },
-    html = { names = false, },
-})
--- nvim/colors/aaron.lua
+++ nvim/colors/aaron.lua
@@ -27,7 +27,7 @@ Color.new("background", '#000000')
 -- MARKDOWN
 Group.new("@text.title.1.markdown", c.green)
 Group.new("@text.title.2.markdown", c.purple)
-Group.new("@text.title.3.markdown", c.orange)
+Group.new("@text.title.3.markdown", c.light_white)
 
 -- COMPLETION
 Group.new("PmenuSel", c.none, c.black)
