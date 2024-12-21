0a7d91b3919fec0d4c2d05159aa58a53580ffba6
aaronshahriari <aaronshahriari@gmail.com>
2024-12-21 06:00:07 -0500

    blink back to cmp

--- nvim/lua/custom/plugins/cmp.lua
+++ nvim/lua/custom/plugins/cmp.lua
@@ -1,35 +1,35 @@
 return {
-  --   "hrsh7th/nvim-cmp",
-  --   lazy = false,
-  --   priority = 100,
-  --   dependencies = {
-  --     "onsails/lspkind.nvim",
-  --     "hrsh7th/cmp-nvim-lsp",
-  --     "hrsh7th/cmp-path",
-  --     "hrsh7th/cmp-buffer",
-  --     { "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
-  --     "saadparwaiz1/cmp_luasnip",
-  --   },
-  --
-  --   config = function()
-  --     local cmp = require("cmp")
-  --
-  --     cmp.setup {
-  --       snippet = {
-  --         expand = function(args)
-  --           require 'luasnip'.lsp_expand(args.body)
-  --         end
-  --       },
-  --       mapping = {
-  --         ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
-  --         ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
-  --         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  --       },
-  --       sources = {
-  --         { name = "nvim_lsp" },
-  --         { name = "path" },
-  --         { name = "buffer" },
-  --       },
-  --     }
-  --   end
+  "hrsh7th/nvim-cmp",
+  lazy = false,
+  priority = 100,
+  dependencies = {
+    "onsails/lspkind.nvim",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-buffer",
+    { "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
+    "saadparwaiz1/cmp_luasnip",
+  },
+
+  config = function()
+    local cmp = require("cmp")
+
+    cmp.setup {
+      snippet = {
+        expand = function(args)
+          require 'luasnip'.lsp_expand(args.body)
+        end
+      },
+      mapping = {
+        ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
+        ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+      },
+      sources = {
+        { name = "nvim_lsp" },
+        { name = "path" },
+        { name = "buffer" },
+      },
+    }
+  end
 }
