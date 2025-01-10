6c18d0cbceb4d1d6eba15a8986195e9a1167abd7
Aaron Shahriari <aaronshahriari@gmail.com>
2025-01-10 05:07:23 -0500

    updated cmp to contain path completion

--- nvim/lua/custom/plugins/cmp.lua
+++ nvim/lua/custom/plugins/cmp.lua
@@ -36,6 +36,7 @@ return {
         ["<C-Space>"] = cmp.mapping.complete(),
       }),
       sources = cmp.config.sources({
+        { name = 'path' },
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
       }, {
