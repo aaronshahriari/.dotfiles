dcd5b924efe67d13fafea35213c5720b5cc78948
Aaron Shahriari <aaronshahriari@gmail.com>
2025-01-10 05:54:50 -0500

    removed lightdm-autorandr whatever that is; update powerbutton color; updated display-sessionizer to run dynamically and have swap

--- scripts/display-sessionizer.sh
+++ scripts/display-sessionizer.sh
@@ -1,33 +1,18 @@
 #!/usr/bin/env bash
 
-case "$(printf "Home\n" | dmenu -i -l 10 -p "Display:")" in
-    "Home")
-        case "$(printf "Laptop\nSingle\nDouble\n" | dmenu -i -l 10 -p "Display Setup:")" in
-            "Laptop")
-                autorandr --load laptop
-                feh --bg-fill /usr/share/backgrounds/desktop_wall.jpg --bg-fill /usr/share/backgrounds/desktop_wall.jpg
-                xinput set-prop "pointer:PIXA3854:00 093A:0274 Touchpad" "libinput Scrolling Pixel Distance" 50
-                xinput set-prop "pointer:PIXA3854:00 093A:0274 Touchpad" "libinput Tapping Enabled" 0
-                xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8 ;;
-            # "Single")
-            #     # set auto setup
-            #     autorandr --load home-single
-            #     feh --bg-fill /usr/share/backgrounds/desktop_wall.jpg --bg-fill /usr/share/backgrounds/desktop_wall.jpg
-            #     # set dpi for home setup
-            #     xinput set-prop "pointer:PIXA3854:00 093A:0274 Touchpad" "libinput Scrolling Pixel Distance" 50
-            #     xinput set-prop "pointer:PIXA3854:00 093A:0274 Touchpad" "libinput Tapping Enabled" 0
-            #     xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8 ;;
-            "Double")
-                # active_monitors=$(xrandr --listmonitors | awk '{print $4}' | tail -n +2)
-                active_monitors=$(xrandr | grep " connected" | awk '{print $1}')
+# get the current set autorandr setups
+autorandr_profiles=$(autorandr | tr ' ' '\n')
 
-                if [[ "$active_monitors" == *"DisplayPort-5"* && "$active_monitors" == *"DisplayPort-4"* ]]; then
-                    autorandr --load home-two2
-                elif [[ "$active_monitors" == *"DisplayPort-7"* && "$active_monitors" == *"DisplayPort-6"* ]]; then
-                    autorandr --load home-two
-                fi
-                feh --bg-fill /usr/share/backgrounds/desktop_wall.jpg --bg-fill /usr/share/backgrounds/desktop_wall.jpg
-                xinput set-prop "pointer:PIXA3854:00 093A:0274 Touchpad" "libinput Scrolling Pixel Distance" 50
-                xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8 ;;
-        esac
+case "$(printf "$autorandr_profiles" | dmenu -i -l 10 -p "Display Setup:")" in
+    "laptop")
+        autorandr --load laptop
+        xinput set-prop "pointer:PIXA3854:00 093A:0274 Touchpad" "libinput Tapping Enabled" 0 ;;
+    "home-two")
+        autorandr --load home-two ;;
+    "home-two-swap")
+        autorandr --load home-two-swap ;;
 esac
+
+feh --bg-fill /usr/share/backgrounds/desktop_wall.jpg --bg-fill /usr/share/backgrounds/desktop_wall.jpg
+xinput set-prop "pointer:PIXA3854:00 093A:0274 Touchpad" "libinput Scrolling Pixel Distance" 50
+xinput set-prop "pointer:Logitech G502" "libinput Accel Speed" -0.8
--- scripts/dwmblocks/power-button.sh
+++ scripts/dwmblocks/power-button.sh
@@ -2,9 +2,11 @@
 
 case $BLOCK_BUTTON in
     1) ~/.local/bin/scripts/device-sessionizer.sh ;;
+    2) ~/.local/bin/scripts/display-sessionizer.sh ;;
+    3) ~/.local/bin/scripts/fix-devices.sh ;;
 	6) setsid -f "ghostty" -e "nvim" "$0" ;;
 esac
 
 power_icon="⏻"
 
-printf "^c#ffffff^%s ^d^" "$power_icon"
+printf "^c#6e8387^%s ^d^" "$power_icon"
