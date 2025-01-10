214af71c523c0a0b0b6bf7b5865466e024c7bcef
Aaron Shahriari <aaronshahriari@gmail.com>
2025-01-10 05:36:04 -0500

    updated to device-sessionizer not fix-devices

--- scripts/dwmblocks/power-button.sh
+++ scripts/dwmblocks/power-button.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 case $BLOCK_BUTTON in
-    1) ~/.local/bin/scripts/fix-devices.sh ;;
+    1) ~/.local/bin/scripts/device-sessionizer.sh ;;
 	6) setsid -f "ghostty" -e "nvim" "$0" ;;
 esac
 
 power_icon="⏻"
 
-printf "^c#ffffff^%s^d^" "$power_icon"
+printf "^c#ffffff^%s ^d^" "$power_icon"
