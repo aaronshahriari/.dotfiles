cf9be6bf3a3825c0f9e40535e8cdd135ae8c4e86
Aaron Shahriari <aaronshahriari@gmail.com>
2025-01-21 03:52:02 -0500

    testing using i3-msg to reset my workspaces when switching configs ( might need to do something more dynamic but )

--- scripts/display-sessionizer.sh
+++ scripts/display-sessionizer.sh
@@ -8,9 +8,35 @@ case "$(printf "$autorandr_profiles" | dmenu -g 1 -i -l 10 -p "Display Setup:")"
         autorandr --load laptop
         xinput set-prop "pointer:PIXA3854:00 093A:0274 Touchpad" "libinput Tapping Enabled" 0 ;;
     "home-two")
-        autorandr --load home-two ;;
+        autorandr --load home-two
+        primary="DisplayPort-5"
+        secondary="DisplayPort-4"
+        i3-msg "workspace 1; move workspace to output $primary"
+        i3-msg "workspace 2; move workspace to output $primary"
+        i3-msg "workspace 3; move workspace to output $primary"
+        i3-msg "workspace 4; move workspace to output $primary"
+        i3-msg "workspace 5; move workspace to output $primary"
+        i3-msg "workspace 6; move workspace to output $secondary"
+        i3-msg "workspace 7; move workspace to output $secondary"
+        i3-msg "workspace 8; move workspace to output $secondary"
+        i3-msg "workspace 9; move workspace to output $secondary"
+        i3-msg "workspace 10; move workspace to output $secondary"
+        ;;
     "home-two-swap")
-        autorandr --load home-two-swap ;;
+        autorandr --load home-two-swap
+        primary="DisplayPort-4"
+        secondary="DisplayPort-5"
+        i3-msg "workspace 1; move workspace to output $primary"
+        i3-msg "workspace 2; move workspace to output $primary"
+        i3-msg "workspace 3; move workspace to output $primary"
+        i3-msg "workspace 4; move workspace to output $primary"
+        i3-msg "workspace 5; move workspace to output $primary"
+        i3-msg "workspace 6; move workspace to output $secondary"
+        i3-msg "workspace 7; move workspace to output $secondary"
+        i3-msg "workspace 8; move workspace to output $secondary"
+        i3-msg "workspace 9; move workspace to output $secondary"
+        i3-msg "workspace 10; move workspace to output $secondary"
+        ;;
 esac
 
 feh --bg-fill /usr/share/backgrounds/desktop_wall.jpg --bg-fill /usr/share/backgrounds/desktop_wall.jpg
