8995d00b35b851606f45643b8cbfa6ac9a6ce653
aaronshahriari_work <ashahriari@brundagegroup.com>
2025-01-22 08:43:11 -0500

    updated removing todo with highlight crap

--- nvim/lua/custom/plugins/todo.lua
+++ nvim/lua/custom/plugins/todo.lua
@@ -1,7 +1,7 @@
 return {
-  "folke/todo-comments.nvim",
-  config = function()
-    local todo = require("todo-comments")
-    todo.setup({})
-  end
+  -- "folke/todo-comments.nvim",
+  -- config = function()
+  --   local todo = require("todo-comments")
+  --   todo.setup({})
+  -- end
 }
