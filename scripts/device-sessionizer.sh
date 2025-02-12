0510d2d6b85ea977de79ae0b6f8c1b22d9cdb637
Aaron Shahriari <aaronshahriari@gmail.com>
2025-02-12 06:05:14 -0500

    updated i3 lock and sessionizer so that when locking usues xautolock

--- scripts/device-sessionizer.sh
+++ scripts/device-sessionizer.sh
@@ -3,11 +3,10 @@
 case "$(printf "Lock\nSleep\nSwitch User\nReboot\nShutdown\n" | dmenu -g 1 -i -l 10 -p "System:")" in
     "Lock")
         sleep 3
-        i3lock-fancy-rapid 10 10
+        i3lock -i /usr/share/backgrounds/lockscreen.jpg
         ;;
     "Sleep")
-        i3lock-fancy-rapid 10 10
-        systemctl suspend
+        xautolock -locknow
         ;;
     "Switch User")
         case "$(printf "Yes\nNo" | dmenu -g 1 -i -l 10 -p "Are You Sure:")" in
