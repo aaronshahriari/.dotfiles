875baca73af13389a473e36ef8515fab0718db13
Aaron Shahriari <aaronshahriari@gmail.com>
2025-02-16 11:16:36 -0500

    updated to display a nice interface when selecting audio to mute

--- scripts/mute-sessionizer.sh
+++ scripts/mute-sessionizer.sh
@@ -18,11 +18,11 @@ if [ -z "$streams" ]; then
 fi
 
 # select stream using dmenu
-selected_stream=$(echo "$streams" | dmenu -i -g 1 -p "Select Stream:")
+selected_stream=$(echo "$streams" | cut -d' ' -f2- | tr -d '\n' | xargs | awk '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) substr($i,2)}1' | dmenu -i -g 1 -p "Select Stream:")
 
 # set the selected sink as default if a valid sink is selected
 if [ -n "$selected_stream" ]; then
-    stream_id=$(echo "$selected_stream" | awk '{print $1}' | sed 's/\.$//')
+    stream_id=$(echo "$streams" | grep "$selected_stream" | awk -F. '{print $1}')
     wpctl set-mute "$stream_id" toggle
     notify-send "Toggle Mute $(echo "$selected_stream" | cut -d' ' -f2- | tr -d '\n' | xargs | awk '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) substr($i,2)}1')"
 else
