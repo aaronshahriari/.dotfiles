c35f63065a4d4d1b4bdd27389c17e050c71ab834
Aaron Shahriari <aaronshahriari@gmail.com>
2025-02-16 08:08:05 -0500

    updated with name for toggle

--- nvim/lua/custom/plugins/toggleterm.lua
+++ nvim/lua/custom/plugins/toggleterm.lua
@@ -10,7 +10,7 @@ return {
       persist_mode = true,
       auto_scroll = false,
     })
-    vim.keymap.set("n", "<C-p>", "<Cmd>ToggleTerm size=90 direction=vertical<CR>")
-    vim.keymap.set("n", "<C-g>", "<Cmd>ToggleTerm size=15 direction=horizontal<CR>")
+    vim.keymap.set("n", "<C-p>", "<Cmd>ToggleTerm size=90 direction=vertical name=vertical<CR>")
+    vim.keymap.set("n", "<C-g>", "<Cmd>ToggleTerm size=15 direction=horizontal name=horizontal<CR>")
   end
 }
