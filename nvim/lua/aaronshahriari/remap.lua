48dfca865214a5ebc52ed950129be0070000da8f
Aaron Shahriari <aaronshahriari@gmail.com>
2025-02-22 06:16:24 -0500

    updated remap and removed xset

--- nvim/lua/aaronshahriari/remap.lua
+++ nvim/lua/aaronshahriari/remap.lua
@@ -63,54 +63,54 @@ vim.keymap.set("t", "<ESC>", [[<C-\><C-n>]])
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww ~/.local/bin/scripts/tmux-sessionizer.sh<CR>")
 
 -- define a function for creating a small terminal
--- vim.keymap.set("n", "<leader>st", function()
---   vim.cmd.new()
---   vim.cmd.term()
---   vim.cmd.wincmd("J")
---   vim.api.nvim_win_set_height(0, 12)
--- end)
+vim.keymap.set("n", "<leader>st", function()
+  vim.cmd.new()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 12)
+end)
 
 -- Define a function for creating a split terminal
--- vim.keymap.set("n", "<leader>t", function()
---   vim.cmd.new()
---   vim.cmd.term()
---   vim.cmd.wincmd("L")
--- end)
+vim.keymap.set("n", "<leader>t", function()
+  vim.cmd.new()
+  vim.cmd.term()
+  vim.cmd.wincmd("L")
+end)
 
 -- function to create term
-local term_bufnr = nil
-local term_win_id = nil
-local function toggle_terminal(position, size)
-  if term_win_id and vim.api.nvim_win_is_valid(term_win_id) then
-    -- if terminal is open, close it
-    vim.api.nvim_win_close(term_win_id, true)
-    term_win_id = nil
-  else
-    if not term_bufnr or not vim.api.nvim_buf_is_valid(term_bufnr) then
-      -- create terminal buffer only if it doesn't exist
-      vim.cmd("new")                              -- open a new split
-      term_bufnr = vim.api.nvim_get_current_buf() -- get the buffer number
-      vim.cmd("term")                             -- start terminal in the buffer
-    else
-      -- open an existing terminal buffer in a new window
-      vim.cmd("new")                          -- open a new split
-      vim.api.nvim_win_set_buf(0, term_bufnr) -- attach existing buffer
-    end
-    -- position the terminal window
-    vim.cmd("wincmd " .. position) -- move it (j = bottom, l = right)
-    if size then
-      vim.api.nvim_win_set_height(0, size)
-    end
-
-    -- store the terminal window id
-    term_win_id = vim.api.nvim_get_current_win()
-  end
-end
-
-vim.keymap.set("n", "<C-g>", function() toggle_terminal("J", 15) end)
-vim.keymap.set("n", "<C-p>", function() toggle_terminal("L", 80) end)
-
--- source lua file
-vim.keymap.set("n", "<leader><leader>", function()
-  vim.cmd("so")
-end)
+-- local term_bufnr = nil
+-- local term_win_id = nil
+-- local function toggle_terminal(position, size)
+--   if term_win_id and vim.api.nvim_win_is_valid(term_win_id) then
+--     -- if terminal is open, close it
+--     vim.api.nvim_win_close(term_win_id, true)
+--     term_win_id = nil
+--   else
+--     if not term_bufnr or not vim.api.nvim_buf_is_valid(term_bufnr) then
+--       -- create terminal buffer only if it doesn't exist
+--       vim.cmd("new")                              -- open a new split
+--       term_bufnr = vim.api.nvim_get_current_buf() -- get the buffer number
+--       vim.cmd("term")                             -- start terminal in the buffer
+--     else
+--       -- open an existing terminal buffer in a new window
+--       vim.cmd("new")                          -- open a new split
+--       vim.api.nvim_win_set_buf(0, term_bufnr) -- attach existing buffer
+--     end
+--     -- position the terminal window
+--     vim.cmd("wincmd " .. position) -- move it (j = bottom, l = right)
+--     if size then
+--       vim.api.nvim_win_set_height(0, size)
+--     end
+--
+--     -- store the terminal window id
+--     term_win_id = vim.api.nvim_get_current_win()
+--   end
+-- end
+--
+-- vim.keymap.set("n", "<C-g>", function() toggle_terminal("J", 15) end)
+-- vim.keymap.set("n", "<C-p>", function() toggle_terminal("L", 80) end)
+--
+-- -- source lua file
+-- vim.keymap.set("n", "<leader><leader>", function()
+--   vim.cmd("so")
+-- end)
