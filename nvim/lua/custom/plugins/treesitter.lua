ec283cc2bd32e0550728dad44279168a584adcae
Aaron Shahriari <aaronshahriari@gmail.com>
2025-03-08 16:41:27 -0500

    hopefully the last push

--- nvim/lua/custom/plugins/treesitter.lua
+++ nvim/lua/custom/plugins/treesitter.lua
@@ -5,16 +5,8 @@ return {
     config = function()
       require("nvim-treesitter.configs").setup({
         -- A list of parser names, or "all"
-        ensure_installed = {
-          "vimdoc", "javascript", "typescript", "c", "lua", "rust",
-          "jsdoc", "bash",
-        },
-
-        -- Install parsers synchronously (only applied to `ensure_installed`)
+        ensure_installed = { "html", "markdown_inline", "java", "python", "javascript", "c", "lua", "vim", "vimdoc", "query" },
         sync_install = false,
-
-        -- Automatically install missing parsers when entering buffer
-        -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
         auto_install = true,
 
         indent = {
