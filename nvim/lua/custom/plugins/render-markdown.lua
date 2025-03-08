316381c89cd3b4d4a680bb62b3ae7a2287c7dcf4
Aaron Shahriari <aaronshahriari@gmail.com>
2025-03-08 16:32:44 -0500

    update with fixed bug

--- nvim/lua/custom/plugins/render-markdown.lua
+++ nvim/lua/custom/plugins/render-markdown.lua
@@ -1,41 +1,41 @@
 return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-  ---@module "render-markdown"
-  ---@type render.md.UserConfig
-  config = function()
-    local render = require("render-markdown")
-    render.setup({
-      heading = { enabled = false, },
-      paragraph = { enabled = false, },
-      code = { enabled = false, },
-      dash = { enabled = false, },
-      bullet = { enabled = false, },
-      quote = { enabled = false, },
-      pipe_table = { enabled = false, },
-      link = { enabled = false, },
-      sign = { enabled = false, },
-      indent = { enabled = false, },
-      checkbox = {
-        enabled = true,
-        position = 'inline',
-        unchecked = {
-          icon = ' ',
-          scope_highlight = nil,
-        },
-        checked = {
-          icon = ' ',
-          scope_highlight = nil,
-        },
-        -- custom = {
-        --   todo = {
-        --     raw = '[-]',
-        --     rendered = '󰥔 ',
-        --     highlight = 'RenderMarkdownTodo',
-        --     scope_highlight = nil
-        --   },
-        -- },
-      },
-    })
-  end
+  -- "MeanderingProgrammer/render-markdown.nvim",
+  -- dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+  -- ---@module "render-markdown"
+  -- ---@type render.md.UserConfig
+  -- config = function()
+  --   local render = require("render-markdown")
+  --   render.setup({
+  --     heading = { enabled = false, },
+  --     paragraph = { enabled = false, },
+  --     code = { enabled = false, },
+  --     dash = { enabled = false, },
+  --     bullet = { enabled = false, },
+  --     quote = { enabled = false, },
+  --     pipe_table = { enabled = false, },
+  --     link = { enabled = false, },
+  --     sign = { enabled = false, },
+  --     indent = { enabled = false, },
+  --     checkbox = {
+  --       enabled = true,
+  --       position = 'inline',
+  --       unchecked = {
+  --         icon = ' ',
+  --         scope_highlight = nil,
+  --       },
+  --       checked = {
+  --         icon = ' ',
+  --         scope_highlight = nil,
+  --       },
+  --       -- custom = {
+  --       --   todo = {
+  --       --     raw = '[-]',
+  --       --     rendered = '󰥔 ',
+  --       --     highlight = 'RenderMarkdownTodo',
+  --       --     scope_highlight = nil
+  --       --   },
+  --       -- },
+  --     },
+  --   })
+  -- end
 }
