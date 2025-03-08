42f09d93d96fd795111df4a2de1d8fee019f112d
Aaron Shahriari <aaronshahriari@gmail.com>
2025-03-08 16:36:44 -0500

    updated with html install

--- nvim/lua/custom/plugins/treesitter.lua
+++ nvim/lua/custom/plugins/treesitter.lua
@@ -1,11 +1,10 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  tag = "v0.9.2",
   build = ":TSUpdate",
   config = function()
     local tree = require("nvim-treesitter.configs")
     tree.setup {
-      ensure_installed = { "markdown_inline", "java", "python", "javascript", "c", "lua", "vim", "vimdoc", "query" },
+      ensure_installed = { "html", "markdown_inline", "java", "python", "javascript", "c", "lua", "vim", "vimdoc", "query" },
       sync_install = false,
       auto_install = true,
       highlight = {
