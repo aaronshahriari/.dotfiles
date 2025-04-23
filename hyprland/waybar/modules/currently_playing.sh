1b05671673c12ba6e3249c5f99a231fd19a2dfa7
aaronshahriari <aaronshahriari@gmail.com>
2025-04-23 17:35:30 -0400

    updated waybar, looking pretty close to almost perfect

--- hyprland/waybar/modules/currently_playing.sh
+++ hyprland/waybar/modules/currently_playing.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-player_status="$(playerctl metadata 2>&1)"
+class=$(playerctl metadata --player=spotify --format '{{lc(status)}}')
+info=$(playerctl metadata --player=spotify --format '{{artist}} - {{title}}')
 
-if [ "$player_status" = "No players found" ]; then
-    printf ""
-else
-    curr_song="$(playerctl metadata --format '{{ artist }} - {{ title }}' | tr -d '\r' | sed 's/[^[:print:]]//g' | awk '{$1=$1; print}')"
-    curr_song_escaped=$(echo "$curr_song" | sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g; s/"/\&quot;/g; s/'"'"'/\&apos;/g')
-    printf "%s\n" "$curr_song_escaped"
+if [[ ${#info} -gt 40 ]]; then
+    info=$(echo $info | cut -c1-40)"..."
 fi
+text=$info
+
+echo -e "{\"text\":\""$text"\", \"class\":\""$class"\"}"
