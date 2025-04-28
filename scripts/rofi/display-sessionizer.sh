5d136c02301ee111fb2fcacfac13b879ad24a2df
aaronshahriari <aaronshahriari@gmail.com>
2025-04-28 06:00:56 -0400

    updated rofi again

--- scripts/rofi/display-sessionizer.sh
+++ scripts/rofi/display-sessionizer.sh
@@ -4,7 +4,7 @@
 autorandr_profiles=$(autorandr | awk '{print $1}')
 
 # use ~/.config/rofi/launchers/scripts/launcher.sh to select one
-selected_display=$(printf "%s\n" "$autorandr_profiles" | ~/.config/rofi/launchers/scripts/launcher.sh -m 0 -g 1 -i -l 10 -p "Display Setup")
+selected_display=$(printf "%s\n" "$autorandr_profiles" | ~/.config/rofi/launchers/scripts/launcher.sh -g 1 -i -l 10 -p "Display Setup")
 
 # if a selection was made, load it
 if [ -n "$selected_display" ]; then
