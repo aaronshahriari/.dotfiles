723d8303eabde3696c2c3ca159de7c2ec9793896
aaronshahriari <aaronshahriari@gmail.com>
2025-04-29 03:31:11 -0400

    removed wayland centering for device-sessionizer

--- scripts/rofi/device-sessionizer.sh
+++ scripts/rofi/device-sessionizer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-case "$(printf "Lock\nSleep\nReboot\nShutdown\n" | ~/.config/rofi/launchers/scripts/launcher.sh -m 0 -g 1 -i -l 10 -p "System")" in
+case "$(printf "Lock\nSleep\nReboot\nShutdown\n" | ~/.config/rofi/launchers/scripts/launcher.sh -g 1 -i -l 10 -p "System")" in
     "Lock")
         i3lock -n -i "$HOME"/Pictures/Wallpapers/lockscreen.png
         ;;
@@ -8,12 +8,12 @@ case "$(printf "Lock\nSleep\nReboot\nShutdown\n" | ~/.config/rofi/launchers/scri
         systemctl suspend
         ;;
     "Reboot")
-        case "$(printf "Yes\nNo" | ~/.config/rofi/launchers/scripts/launcher.sh -m 0 -g 1 -i -l 10 -p "Are You Sure")" in
+        case "$(printf "Yes\nNo" | ~/.config/rofi/launchers/scripts/launcher.sh -g 1 -i -l 10 -p "Are You Sure")" in
             "Yes") reboot ;;
             *) exit 1 ;;
         esac ;;
     "Shutdown")
-        case "$(printf "Yes\nNo" | ~/.config/rofi/launchers/scripts/launcher.sh -m 0 -g 1 -i -l 10 -p "Are You Sure")" in
+        case "$(printf "Yes\nNo" | ~/.config/rofi/launchers/scripts/launcher.sh -g 1 -i -l 10 -p "Are You Sure")" in
             "Yes") shutdown -h now ;;
             *) exit 1 ;;
         esac ;;
