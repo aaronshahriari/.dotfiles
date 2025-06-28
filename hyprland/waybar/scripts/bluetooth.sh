39a1794ce7ec5e41b1688fefffd35c376fba3896
aaronshahriari <aaronshahriari@gmail.com>
2025-06-28 16:09:56 -0400

    updated waybar config to tooltip the current bluetooth and mic devices

--- hyprland/waybar/scripts/bluetooth.sh
+++ hyprland/waybar/scripts/bluetooth.sh
@@ -3,7 +3,9 @@
 bt_powered=$(bluetoothctl show 2>/dev/null | grep "Powered:" | awk '{print $2}')
 
 if [[ "$bt_powered" == "yes" ]]; then
+    device=$(bluetoothctl devices Connected | cut -d ' ' -f3- | head -n 1)
     echo "󰂯"
+    echo "$device"
 elif [[ "$bt_powered" == "no" ]]; then
     echo ""
 else
--- hyprland/waybar/scripts/voice.sh
+++ hyprland/waybar/scripts/voice.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+    device=$(wpctl inspect @DEFAULT_AUDIO_SOURCE@ | grep 'node.description' | cut -d '"' -f2)
 if wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | grep -q '\[MUTED\]'; then
     echo " "
 else
     echo ""
 fi
+echo "$device"
