6ff1803f56538a87c462e7fdbdaafaf607919f68
aaronshahriari <aaronshahriari@gmail.com>
2025-07-04 07:57:17 -0400

    wip

--- nvim/lua/aaronshahriari/remap.lua
+++ nvim/lua/aaronshahriari/remap.lua
@@ -71,6 +71,7 @@ vim.keymap.set("t", "<ESC>", [[<C-\><C-n>]])
 
 -- run tmux inside of vim
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww ~/.local/bin/scripts/tmux-sessionizer.sh<CR>")
+vim.keymap.set("n", "<C-m>", "<cmd>silent !~/.local/bin/scripts/ai-sessionizer.sh<CR>")
 
 -- source lua file
 vim.keymap.set("n", "<leader><leader>", function()
