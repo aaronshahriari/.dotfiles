11c6b63500170f87bc963b0e1d68f863ee19233d
aaronshahriari <aaronshahriari@gmail.com>
2025-07-04 08:16:25 -0400

    fix devices update to run rsync

--- scripts/rofi/fix-devices.sh
+++ scripts/rofi/fix-devices.sh
@@ -1,58 +1,20 @@
 #!/usr/bin/env bash
 
-reset_cam() {
-    cam_id=$(lsusb | grep 'C920 PRO HD Webcam' | cut -d' ' -f6)
-    if [ -n "$cam_id" ]; then
-        sudo usbreset "$cam_id"
-    else
-        echo "Webcam could not be found" >&2
-        return 1
-    fi
+ROFI_LAUNCHER="$HOME/.config/rofi/launchers/scripts/launcher.sh"
+SCRIPT_DIR="$HOME/.local/bin/scripts"
 
-}
+options="Wallpaper\nBackup"
 
-bluelight_on() {
-    hyprsunset=$(pgrep hyprsunset)
-    if [ -z "$hyprsunset" ]; then
-        hyprsunset &
-        hyprctl hyprsunset temperature 2500
-    else
-        hyprctl hyprsunset temperature 2500
-    fi
-}
+choice=$(printf "$options" | $ROFI_LAUNCHER -g 1 -m 0 -i -l 10 -p "Run")
 
-bluelight_off() {
-    hyprsunset=$(pgrep hyprsunset)
-    if [ -z "$hyprsunset" ]; then
-        hyprsunset &
-        hyprctl hyprsunset identity
-    else
-        hyprctl hyprsunset identity
-    fi
-}
-
-case "$(printf "Hyprsunset\nWallpaper\nLogitech Mouse\nWireplumber\n" | ~/.config/rofi/launchers/scripts/launcher.sh -g 1 -m 0 -i -l 10 -p "Run")" in
-    "Hyprsunset")
-        case "$(printf "On\nOff\n" | ~/.config/rofi/launchers/scripts/launcher.sh -g 1 -m 0 -i -l 10)" in
-            "On")
-                bluelight_on
-                ;;
-            "Off")
-                bluelight_off
-                ;;
-        esac
-        ;;
-
+case "$choice" in
     "Wallpaper")
-        "$HOME"/.config/hypr/modules/wallpaper.sh
+        "$HOME/.config/hypr/modules/wallpaper.sh"
         ;;
-
-    "Logitech Camera")
-        reset_cam
+    "Backup")
+        "$SCRIPT_DIR/backup-script.sh"
         ;;
-
-    "Wireplumber")
-        systemctl --user restart wireplumber.service
-        systemctl --user restart pipewire.service
+    *)
+        exit 0
         ;;
 esac
