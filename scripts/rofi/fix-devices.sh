a176d7ba7c7f43b12f9a929541169cc75a94ea58
aaronshahriari <aaronshahriari@gmail.com>
2025-07-16 16:45:29 -0400

    updated fix-devices for camera

--- scripts/rofi/fix-devices.sh
+++ scripts/rofi/fix-devices.sh
@@ -3,7 +3,18 @@
 ROFI_LAUNCHER="$HOME/.config/rofi/launchers/scripts/launcher.sh"
 SCRIPT_DIR="$HOME/.local/bin/scripts"
 
-options="Wallpaper\nBackup"
+reset_cam() {
+    cam_id=$(lsusb | grep 'C920 PRO HD Webcam' | cut -d' ' -f6)
+    if [ -n "$cam_id" ]; then
+        sudo usbreset "$cam_id"
+    else
+        echo "Webcam could not be found" >&2
+        return 1
+    fi
+
+}
+
+options="Wallpaper\nBackup\nLogitech Camera"
 
 choice=$(printf "$options" | $ROFI_LAUNCHER "Run:")
 
@@ -13,6 +24,9 @@ case "$choice" in
         ;;
     "Backup")
         "$SCRIPT_DIR/backup-script.sh"
+        ;;
+    "Logitech Camera")
+        reset_cam
         ;;
     *)
         exit 0
