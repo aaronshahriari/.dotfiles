7f5f70bd6c357538ff62a03711b25c212c86aa0d
aaronshahriari <aaronshahriari@gmail.com>
2025-08-06 17:15:33 -0400

    cleansing the palet

--- hyprland/hypr/scripts/screenshot.sh
+++ hyprland/hypr/scripts/screenshot.sh
@@ -2,7 +2,35 @@
 
 DIR="$HOME/Pictures/Screenshots"
 mkdir -p "$DIR"
-
 FILENAME="$DIR/screenshot_$(date +%Y-%m-%d_%H-%M-%S).png"
 
-grim -g "$(slurp)" "$FILENAME" || exit 1
+capture() {
+    pkill slurp || hyprshot -m ${1:-region} --raw |
+        satty --filename - \
+        --output-filename "$FILENAME" \
+        --early-exit \
+        --actions-on-enter save-to-clipboard \
+        --save-after-copy \
+        --copy-command 'wl-copy'
+}
+
+options="Window\nSelect\nOutput"
+screenshot_type=$(printf "$options" | ~/.config/rofi/launchers/scripts/launcher.sh "Type:")
+
+# close if no selection
+[ -z "$screenshot_type" ] && exit
+
+case "$screenshot_type" in
+    "Window")
+        capture window
+        ;;
+    "Select")
+        capture region
+        ;;
+    "Output")
+        capture output
+        ;;
+    *)
+        exit 0
+        ;;
+esac
--- scripts/rofi/device-sessionizer.sh
+++ scripts/rofi/device-sessionizer.sh
@@ -1,20 +1,44 @@
 #!/usr/bin/env bash
-
-case "$(printf "Lock\nSleep\nReboot\nShutdown\n" | ~/.config/rofi/launchers/scripts/launcher.sh "System:")" in
+case "$(printf "Lock\nSuspend\nLogout\nReboot\nShutdown\n" | ~/.config/rofi/launchers/scripts/launcher.sh "System:")" in
     "Lock")
-        i3lock -n -i "$HOME"/Pictures/Wallpapers/lockscreen.png
+        hyprlock
         ;;
-    "Sleep")
-        systemctl suspend
-        ;;
+    "Suspend")
+        case "$(printf "Yes\nNo" | ~/.config/rofi/launchers/scripts/launcher.sh "Are You Sure:")" in
+            "Yes") 
+                mpc -q pause
+                amixer set Master mute
+                hyprlock &
+                systemctl suspend
+                ;;
+            *) exit 1 ;;
+        esac ;;
+    "Logout")
+        case "$(printf "Yes\nNo" | ~/.config/rofi/launchers/scripts/launcher.sh "Are You Sure:")" in
+            "Yes") 
+                if [[ "$DESKTOP_SESSION" == 'i3' ]]; then
+                    i3-msg exit
+                elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
+                    bspc quit
+                elif [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
+                    openbox --exit
+                elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
+                    qdbus org.kde.ksmserver /KSMServer logout 0 0 0
+                else
+                    # Fallback for other desktop environments
+                    pkill -KILL -u "$USER"
+                fi
+                ;;
+            *) exit 1 ;;
+        esac ;;
     "Reboot")
         case "$(printf "Yes\nNo" | ~/.config/rofi/launchers/scripts/launcher.sh "Are You Sure:")" in
-            "Yes") reboot ;;
+            "Yes") systemctl reboot ;;
             *) exit 1 ;;
         esac ;;
     "Shutdown")
         case "$(printf "Yes\nNo" | ~/.config/rofi/launchers/scripts/launcher.sh "Are You Sure:")" in
-            "Yes") shutdown -h now ;;
+            "Yes") systemctl poweroff ;;
             *) exit 1 ;;
         esac ;;
     *) exit 1 ;;
