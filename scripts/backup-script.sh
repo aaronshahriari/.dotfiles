a568d414bc0ae5ae41169183381d7ab61457d85e
aaronshahriari <aaronshahriari@gmail.com>
2025-08-14 11:02:10 -0400

    updated waybar; updated some notifications for backup

--- scripts/backup-script.sh
+++ scripts/backup-script.sh
@@ -2,6 +2,10 @@
 
 CURRENT_DATE_TIME=$(date "+%Y-%m-%d_%H-%M-%S")
 LOG_FILE="$HOME/backup_logs/${CURRENT_DATE_TIME}-backup.log"
+ICON_PATH="$(nix path-info nixpkgs#material-black-colors)/share/icons/Material-Black-Plum-Numix-FLAT/32"
+STARTING="$ICON_PATH/actions/alarm.svg"
+COMPLETE="$ICON_PATH/status/stock_check-filled.svg"
+ERROR="$ICON_PATH/status/dialog-error.svg"
 
 SOURCE=(
   "$HOME/Videos"
@@ -17,4 +21,10 @@ SOURCE=(
 
 DESTINATION="nas_personal:/volume1/Backups/NixOS-Laptop"
 
-rsync -avuz --delete --log-file="$LOG_FILE" -e ssh "${SOURCE[@]}" "$DESTINATION"
+notify-send -t 3000 -u normal -i "$STARTING" "Starting Rsync Backup"
+if ! rsync -avuz --delete --log-file="$LOG_FILE" -e ssh "${SOURCE[@]}" "$DESTINATION"; then
+    notify-send -t 4000 -u critical -i "$ERROR" "Backup Failed" "rsync backup to NAS failed. Check $LOG_FILE for details."
+    exit 1
+else
+    notify-send -t 3000 -u normal -i "$COMPLETE" "Backup Complete" "rsync backup to NAS completed successfully."
+fi
